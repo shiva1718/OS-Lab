0c5f4b000addacacf6756507d11ac8c9d7c750e4
Sujay <66252042+Sujay-0304@users.noreply.github.com>
2023-06-02 06:27:26 +0000

    b

---
+++ 5/b.c
@@ -0,0 +1,23 @@
+#include<stdio.h> 
+#include<stdlib.h>
+#include<string.h> 
+int main(){
+    char fn[10],pat[10],temp[200];
+    FILE *fp;
+    printf("Enter file name: ");
+    scanf("%s",fn);
+    printf("Enter pattern to be searched: ");
+    scanf("%s",pat);
+    fp=fopen(fn,"r");
+    if(fp==NULL){
+        perror("Cannot open file");
+        exit(-1);
+    }
+    while(!feof(fp)){
+        fgets(temp,200,fp);
+        if(strstr(temp,pat)){
+            printf("%s",temp);
+        } 
+    }
+    fclose(fp);
+}
